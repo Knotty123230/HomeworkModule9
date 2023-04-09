929444fcdcfc998bb28b668fda842d681784c3fb
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-04-09 19:46:33 +0300

    commit

--- src/List/hashmap/MyHashMap.java
+++ src/List/hashmap/MyHashMap.java
@@ -141,19 +141,4 @@ public class MyHashMap<T, V> {
         sb.append("}");
         return sb.toString();
     }
-    public static void main(String[] args) {
-        MyHashMap myHashMap = new MyHashMap();
-        myHashMap.put("1", 1);
-        myHashMap.put("2", 2);
-        myHashMap.put("3", 3);
-        myHashMap.put("4", 4);
-        myHashMap.put("5", 5);
-        myHashMap.put("6", 6);
-        System.out.println("myHashMap = " + myHashMap);
-        System.out.println("myHashMap.get(1) = " + myHashMap.get("1"));
-        myHashMap.remove("1");
-        System.out.println("myHashMap = " + myHashMap);
-        myHashMap.clear();
-        System.out.println("myHashMap = " + myHashMap);
-    }
 }
